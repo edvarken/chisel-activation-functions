b79e5d9d106929dadc81d969149fb53b4d265f57
Edward Mulier <ward.mulier@gmail.com>
2025-04-19 19:53:13 +0200

    correct comments in relu6

--- src/main/scala/silu/relu6.scala
+++ src/main/scala/silu/relu6.scala
@@ -13,21 +13,21 @@ class relu6 extends Module {
   val a  = io.in_a
   // io.out_debug := false.B // normally
 
-  when (a(15).asUInt === 1.U || a === "b0_00000000_0000000".U) { // in_a negative or 0
+  when (a(15).asUInt === 1.U || a === "b0_00000000_0000000".U) { // in_a <= 0
     io.out_a := 0.U // 0_00000000_0000000
 
-  }.elsewhen ((a(14,7)).asUInt >= 130.U(8.W)) { // 6 < in_a
+  }.elsewhen ((a(14,7)).asUInt >= 130.U(8.W)) { // 8 <= in_a
     io.out_a := "b0_10000001_1000000".asUInt(16.W) // 6 = 1.5 * 2^3
     // io.out_debug := true.B
 
-  }.elsewhen (a(14,7).asUInt < 129.U(8.W)){ // 0 < in_a < 6
+  }.elsewhen (a(14,7).asUInt < 129.U(8.W)){ // 0 < in_a < 4
     io.out_a := a // out = in
 
-  }.otherwise { // exponent is 2^2, look at mantissa: if msb is 1, then a >= 6, and thus out=6
-    when (a(6).asUInt === 1.U) {
+  }.otherwise { // 4 <= in_a < 8 (the exponent is 2^2 here) 
+    when (a(6).asUInt === 1.U) { // look at mantissa: if msb is 1, then a >= 6, and thus out=6
       io.out_a := "b0_10000001_1000000".asUInt(16.W) // 6 = 1.5 * 2^3
 
-    }.otherwise {
+    }.otherwise { // 4 <= in_a < 6
       io.out_a := a // out = in
     }
   }
