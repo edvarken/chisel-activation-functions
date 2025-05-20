53652c98026b0497fbed181cd037ccf9c80aac1c
Edward Mulier <ward.mulier@gmail.com>
2025-05-20 19:45:42 +0200

    fix FPMult and its testbench

--- src/main/scala/silu/FPMult.scala
+++ src/main/scala/silu/FPMult.scala
@@ -6,9 +6,11 @@ class MantissaRounder(val n: Int) extends Module {
     val io = IO(new Bundle {
         val in = Input(UInt(n.W))
         val out = Output(UInt((n - 1).W))
+        val carry = Output(Bool())
     })
-
-    io.out := io.in(n - 1, 1) + io.in(0)
+    // if all bits of in are 1, set carry to true, rounder.io.out will be all zeroes which is correct.
+    io.carry := io.in.andR // if all bits are 1, carry is true
+    io.out := io.in(n - 1, 1) + io.in(0) // overflow if: io.out = 1111111(7bits) + 1
 }
 
 class FPMult(val n: Int) extends Module { // single-cycle pipeline latency
@@ -18,14 +20,12 @@ class FPMult(val n: Int) extends Module { // single-cycle pipeline latency
         val res = Output(Bits(n.W))
     })
 
-    // require(Set(16, 32, 64).contains(n), "Only bfloat16, fp32, fp64 supported")require(Set(16, 32, 64).contains(n), "Only bfloat16, fp32, fp64 supported")
-
-    val a_wrap = new FloatWrapper(io.a)
+    val a_wrap = new FloatWrapper(io.a) // (FloatWrapper prepends implicit 1 to mantissa!)
     val b_wrap = new FloatWrapper(io.b)
 
     val stage1_sign = a_wrap.sign.asInstanceOf[UInt] ^ b_wrap.sign.asInstanceOf[UInt]
     val stage1_exponent = a_wrap.exponent.asInstanceOf[UInt] + b_wrap.exponent.asInstanceOf[UInt]
-    val stage1_mantissa = a_wrap.mantissa.asInstanceOf[UInt] * b_wrap.mantissa.asInstanceOf[UInt]
+    val stage1_mantissa = a_wrap.mantissa.asInstanceOf[UInt] * b_wrap.mantissa.asInstanceOf[UInt] // chisel makes multiplier in verilog from this
     val stage1_zero = a_wrap.zero.asInstanceOf[Bool] || b_wrap.zero.asInstanceOf[Bool]
 
     val sign_reg = RegNext(stage1_sign)
@@ -34,35 +34,38 @@ class FPMult(val n: Int) extends Module { // single-cycle pipeline latency
     val zero_reg = RegNext(stage1_zero)
 
     val stage2_sign = sign_reg
-    // val stage2_exponent = UInt((a_wrap.exponent.getWidth).W) // should add Wire() since we only declare it without driving it yet, see correction below
+    // val stage2_exponent = UInt((a_wrap.exponent.getWidth).W) // should add Wire() since we only declare it without driving it yet, see correction in line below
     val stage2_exponent = Wire(UInt((a_wrap.exponent.asInstanceOf[UInt].getWidth).W)) // getWidth comes from FloatWrapper, but is in same package
+    val stage3_exponent = Wire(UInt((a_wrap.exponent.asInstanceOf[UInt].getWidth).W))
     val stage2_mantissa = Wire(UInt((a_wrap.mantissa.asInstanceOf[UInt].getWidth - 1).W))
 
     val (mantissaLead, mantissaSize, exponentSize, exponentSub) = n match {
-        case 16 => (15, 7, 8, 127) // BF16, mantissaLead = 15?
+        case 16 => (15, 7, 8, 127) // BF16, mantissaLead = 15 = 2*7 + 1
         case 32 => (47, 23, 8, 127) // mantissaLead = 2*mantissaSize + 1
         case 64 => (105, 52, 11, 1023) // mantissaLead = 2*mantissaSize + 1
     }
 
-    val rounder = Module(new MantissaRounder(mantissaSize + 1))
+    val rounder = Module(new MantissaRounder(mantissaSize + 1)) // receives 8bits as input
 
-    when (zero_reg) {
+    when (zero_reg) { // if either input is zero, output is zero
         stage2_exponent := 0.U(exponentSize.W)
         rounder.io.in := 0.U((mantissaSize + 1).W)
-    } .elsewhen (mantissa_reg(mantissaLead) === 1.U) {
-        stage2_exponent := exponent_reg - (exponentSub - 1).U
-        rounder.io.in := mantissa_reg(mantissaLead - 1,
-                                      mantissaLead - mantissaSize - 1)
-    } .otherwise {
-        stage2_exponent := exponent_reg - exponentSub.U
-        rounder.io.in := mantissa_reg(mantissaLead - 2,
-                                      mantissaLead - mantissaSize - 2)
-    }
 
+    } .elsewhen (mantissa_reg(mantissaLead) === 1.U) { // if the product of prepended1_mantissa_a * prepended1_mantissa_b has a 1 bit at the front, we need to add 1 to exponent to normalize mantissa
+        stage2_exponent := exponent_reg - (exponentSub - 1).U // If you increment the exponent due to overflow, 
+        // you should also shift the mantissa right by one to normalize it. This is done here(the lsbit starts more to the left, and msbit ends more to the left as well)
+        rounder.io.in := mantissa_reg(mantissaLead - 1, mantissaLead - mantissaSize - 1)
+
+    } .otherwise { 
+        stage2_exponent := exponent_reg - exponentSub.U
+        rounder.io.in := mantissa_reg(mantissaLead - 2, mantissaLead - mantissaSize - 2)
+    }
     stage2_mantissa := rounder.io.out
+    // If mantissa rounding causes overflow, increment the exponent to normalize
+    stage3_exponent := Mux(rounder.io.carry === true.B, stage2_exponent + 1.U, stage2_exponent)
 
     io.res := Cat(stage2_sign.asUInt,
-                  stage2_exponent.asUInt,
+                  stage3_exponent.asUInt,
                   stage2_mantissa.asUInt)
 }
 
--- src/test/scala/silu/FPMultTest.scala
+++ src/test/scala/silu/FPMultTest.scala
@@ -12,30 +12,75 @@ import FloatUtils.{floatToBigInt, floatToBigIntBF16, doubleToBigInt, getExpMantW
 class FPMult16Test extends AnyFreeSpec with Matchers {
     "FPMult16 should correctly multiply floating-point numbers" in {
         simulate(new FPMult16) { c =>
+
+            def toBinary(i: Int, digits: Int = 16) =
+                String.format("%" + digits + "s", i.toBinaryString).replace(' ', '0')
+
             var lastExpected = 0.0f // must be a var and not a val, so we can update it
             c.io.a.poke(floatToBigIntBF16(0.0f).U(16.W))
             c.io.b.poke(floatToBigIntBF16(3.0f).U(16.W))
             c.clock.step(1)
+            assert(floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt == c.io.res.peek().litValue.toInt,
+            s"Expected ${toBinary(floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt, 16)} but got ${toBinary(c.io.res.peek().litValue.toInt, 16)}")
             
-            for (_ <- 0 until 8) {
-                val a = scala.util.Random.nextFloat() * 10000.0f - 5000.0f
+            val input_a = -4.562719f
+            val input_b = 0.43761528f
+            val expected = -1.9967154f
+            val expected_exponent = math.floor(math.log(math.abs(expected))/math.log(2)).toInt
+            val max_diff = math.pow(2, expected_exponent-7).toFloat
+
+            c.io.a.poke(floatToBigIntBF16(input_a).U(16.W))
+            c.io.b.poke(floatToBigIntBF16(input_b).U(16.W))
+            c.clock.step(1)
+            // during usage of this module inside another module, the actual output was wrongly -1.0 instead of -1.9967.
+            // this was due to overflow bug in MantissaRounder, which is fixed now.
+            val actual_value = java.lang.Float.intBitsToFloat((BigInt(c.io.res.peek().litValue.toInt) << 16).toInt)
+            assert(math.abs(actual_value - expected) < max_diff, s"Expected ${toBinary(floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt, 16)} but got ${toBinary(c.io.res.peek().litValue.toInt, 16)}")
+            
+            for (_ <- 0 until 20) {
+                val a = scala.util.Random.nextFloat() * 10000.0f - 5000.0f // [0,1]*10000-5000: -5000 to 5000
                 val b = scala.util.Random.nextFloat() * 10000.0f - 5000.0f
                 val expected = a * b
+                // Quite large errors for numbers with large exponent 
+                // -> just check maxdiff is smaller than 2^(exponent-5), so the error must be smaller than contribution of two lsbits of mantissa
+                val expected_exponent = math.floor(math.log(math.abs(expected))/math.log(2)).toInt
+                val max_diff = math.pow(2, expected_exponent-5).toFloat
 
                 c.io.a.poke(floatToBigIntBF16(a).U)
                 c.io.b.poke(floatToBigIntBF16(b).U)
                 c.clock.step(1)
 
                 lastExpected = expected
-                // c.io.res is 1 or 2 bits off mostly
-                // Largest possible error when truncating FP32 to BF16 is ~ 1E36! Instead check for upper 16 bits being the same
-                assert((c.io.res.peek().litValue.toInt & 0xFFFF0000) == (floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt & 0xFFFF0000),
-                s"Expected ${floatToBigIntBF16(lastExpected).U(16.W)} but got ${c.io.res.peek().litValue.toInt & 0xFFFF0000}")
+                // c.io.res is often 1 or 2 bits off in the mantissa of the bf16 representation
+                // Largest possible error when truncating FP32 to BF16 is ~ 1E36, however error creeps in up to 3rd lsbit of the mantissa!
+                val actual_value = java.lang.Float.intBitsToFloat((BigInt(c.io.res.peek().litValue.toInt) << 16).toInt)
+                println(f"actual_value: ${actual_value}")
+                println(f"expected: ${expected}")
+                assert(math.abs(actual_value - expected) < max_diff, s"Expected ${toBinary(floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt, 16)} but got ${toBinary(c.io.res.peek().litValue.toInt, 16)}")
             }
             c.clock.step(1)
+            for (_ <- 0 until 20) {
+                val a = scala.util.Random.nextFloat() * 14.0f - 7.0f // [0,1]*14-7: -7 to +7
+                val b = scala.util.Random.nextFloat() * 14.0f - 7.0f
+                val expected = a * b
+                val expected_exponent = math.floor(math.log(math.abs(expected))/math.log(2)).toInt
+                val max_diff = math.pow(2, expected_exponent-5).toFloat
+                c.io.a.poke(floatToBigIntBF16(a).U)
+                c.io.b.poke(floatToBigIntBF16(b).U)
+                c.clock.step(1)
+
+                // c.io.res is 1 or 2 bits off mostly
+                // Largest possible error when truncating FP32 to BF16 is ~ 1E36= 2^127*2^-8! Instead check for exponent being right.
+                val actual_value = java.lang.Float.intBitsToFloat((BigInt(c.io.res.peek().litValue.toInt) << 16).toInt)
+                println(f"actual_value: ${actual_value}")
+                println(f"expected: ${expected}")
+                assert(math.abs(actual_value - expected) < max_diff, s"Expected ${toBinary(floatToBigIntBF16(lastExpected).U(16.W).litValue.toInt, 16)} but got ${toBinary(c.io.res.peek().litValue.toInt, 16)}")
+                println(f"expected: ${toBinary(floatToBigIntBF16(expected).U(16.W).litValue.toInt, 16)}, actual: ${toBinary(c.io.res.peek().litValue.toInt, 16)}")
+            }
         }
     }
 }
+
 
 class FPMult32Test extends AnyFreeSpec with Matchers {
     "FPMult32 should correctly multiply floating-point numbers" in {
