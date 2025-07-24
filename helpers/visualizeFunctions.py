043190c3842158dd1b7e2af5117fcd3391825d86
Edward Mulier <ward.mulier@gmail.com>
2025-07-24 19:29:53 +0200

    generate absolute errors plot for N=200 for (-6,6) for silu and gelu 20 non-uniform sigmoid segments

--- helpers/visualizeFunctions.py
+++ helpers/visualizeFunctions.py
@@ -255,7 +255,7 @@ def visualizeGELUAndSiLU():
     x = np.linspace(xmin, xmax, 1000)
     exact_silu = x / (1 + np.exp(-x))
     plt.rcParams['text.usetex'] = True
-    ax.plot(x, exact_silu, label='SiLU', color=colors[0], linestyle='--', linewidth=1.5)
+    ax.plot(x, exact_silu, label='SiLU', color=colors[0], linestyle='-', linewidth=1.5)
 
     # real GELU
     exact_gelu = x * 0.5 * (1 + erf(x / np.sqrt(2)))
@@ -335,5 +335,5 @@ if __name__ == "__main__":
     # visualizeGELUAndApprox()
     # visualizeDyTAndApprox()
 
-    # visualizeGELUAndSiLU()
-    visualizeSigmoid()
+    visualizeGELUAndSiLU()
+    # visualizeSigmoid()
